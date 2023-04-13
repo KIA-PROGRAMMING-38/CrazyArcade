9bcf2cc3ed5572ec30967a5ddb523f8aa7faf5a7
minyeong <minyk0512@gmail.com>
2023-04-13 16:11:38 +0900

    [Modify] 플레이어 애니메이션 수정
    Dying -> Die 부분 수정
    1. 애니메이션 진행 속도
    2. alpha값을 애니메이션이 아닌 StateMachineBehaviour 내에서 코드로 접근하여 수정

--- CrazyArcade/Assets/Scripts/Character/PlayableCharacter/PlayableCharacterDying.cs
+++ CrazyArcade/Assets/Scripts/Character/PlayableCharacter/PlayableCharacterDying.cs
@@ -1,14 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static PlayableCharacter;
 
 public class PlayableCharacterDying : StateMachineBehaviour
 {
-    PlayableCharacter _playableCharacter;
+    private PlayableCharacter _playableCharacter;
+    private SpriteRenderer _spriteRenderer;
+    private Color _color;
+
+    private float _deltaTime;
+    private float _elapsedTime;
+    private float _duration;
+    private float _startAlpha;
+    private float _lastAlpha;
+
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        // 속도 변화
         _playableCharacter = animator.GetComponent<PlayableCharacter>();
         _playableCharacter._savedSpeed = _playableCharacter._speed;
         _playableCharacter._speed = 0.2f;
+
+        // alpha값 변화
+        _spriteRenderer = animator.gameObject.GetComponent<SpriteRenderer>();
+        _startAlpha = 0.7f;
+        _lastAlpha = 1f;
+        _duration = 5f;
+
+        _color = _spriteRenderer.material.color;
+        _color.a = _startAlpha;
+        _spriteRenderer.material.color = _color;
     }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime >= _duration)
+        {
+            animator.SetTrigger(PlayerAnimID.IS_DYING_LAST);
+        }
+
+        _color.a = Mathf.Lerp(_startAlpha, _lastAlpha, _elapsedTime / _duration);
+        _spriteRenderer.material.color = _color;
+    }
+
 }
