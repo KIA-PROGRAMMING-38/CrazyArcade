d4e6227014f44d4e956c77b3ad4e981260a32bdd
minyeong <minyk0512@gmail.com>
2023-04-28 15:57:57 +0900

    [Feat] 일정 비율 이하 HP bar 색상 변경

--- CrazyArcade/Assets/BossMonsterHPBar.cs
+++ CrazyArcade/Assets/BossMonsterHPBar.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class BossMonsterHPBar : MonoBehaviour
 {
     [SerializeField] private Image _progressbar;
+    [SerializeField] private Sprite _progressbarRed;
     [SerializeField] private Collider2D _boss;
 
     private void Update()
@@ -16,6 +17,11 @@ public class BossMonsterHPBar : MonoBehaviour
 
     public void UpdateHPBar(float ratio)
     {
+        if(ratio <= 0.3f)
+        {
+            _progressbar.sprite = _progressbarRed;
+        }
+
         _progressbar.fillAmount = ratio;
     }
 }
