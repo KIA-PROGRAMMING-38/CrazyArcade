f6602574136c28d65072ff56954713ea944a4132
minyeong <minyk0512@gmail.com>
2023-05-02 19:12:27 +0900

    [Fix] 머지 충돌 해결

--- CrazyArcade/Assets/Scripts/Manager/GameManager.cs
+++ CrazyArcade/Assets/Scripts/Manager/GameManager.cs
@@ -10,7 +10,6 @@ public class GameManager : SingletoneBehaviour<GameManager>
     {
         base.Awake();
         DataReader.ReadData();
-        // TODO: 씬 전환 구현 후에는 스테이지 씬으로 전환하는 경우 불러와야 함
         MapManager.GetTotalMapInfo();
         ItemActions.SaveActions();
 
