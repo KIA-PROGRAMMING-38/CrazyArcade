bb918b54207839635c987c9ebe8b90f6e3f507cb
minyeong <minyk0512@gmail.com>
2023-05-02 19:13:04 +0900

    [Fix] 머지 충돌 해결

--- CrazyArcade/Assets/Scripts/Manager/GameManager.cs
+++ CrazyArcade/Assets/Scripts/Manager/GameManager.cs
@@ -13,6 +13,8 @@ public class GameManager : SingletoneBehaviour<GameManager>
         MapManager.GetTotalMapInfo();
         ItemActions.SaveActions();
 
+        Debug.Log("머지 충돌 해결 제발");
+
         PlayersInfo.SavePlayersName("플레이어 1", "플레이어 2");
         PlayersInfo.SavePlayerCharacter("0", "0");
     }
